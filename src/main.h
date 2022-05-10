0f6e26572304a373bb3ffa9e7d32399ade0b377a
Thomas Howland <thomas.howland@verizon.net>
2022-05-10 00:19:05 -0400

    relocated screen size constants added "doomfire.h"

--- src/main.h
+++ src/main.h
@@ -1,5 +1,10 @@
 #pragma once
 
+// Constants FIXME don't leave these here forever
+// Console width and height
+const int CON_W = 80, CON_H = 45;
+const int SCREEN_W = 1280, SCREEN_H = 720;
+
 // Standard libraries
 #include <iostream>
 
@@ -8,3 +13,4 @@
 #include <SDL.h>
 
 // Other
+#include "doom_fire.h"
